0ce6814ef4a75ddc4597cddcb82b347545af0dfc
narandza <60319154+narandza@users.noreply.github.com>
2025-06-17 17:08:26 +0200

    group auth folders

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import { Card } from "@/components/ui/card";
+
+const Page = () => {
+  return <Card>sign in</Card>;
+};
+
+export default Page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import { Card } from "@/components/ui/card";
+
+const Page = () => {
+  return <Card>sign up</Card>;
+};
+
+export default Page;
