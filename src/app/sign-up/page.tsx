7a45991ce649486592721e5501f22e53e09951a0
narandza <60319154+narandza@users.noreply.github.com>
2025-06-17 16:56:00 +0200

    add auth pages routes

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl ">sign up page</div>
+    </div>
+  );
+};
+
+export default Page;
