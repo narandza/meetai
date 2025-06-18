da7d22a17f79d3218df44af98b6c522f630ecb95
narandza <60319154+narandza@users.noreply.github.com>
2025-06-18 18:55:55 +0200

    Add ToS and PP template

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -126,6 +126,11 @@ export const SignInView = () => {
           </div>
         </CardContent>
       </Card>
+
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>
+      </div>
     </div>
   );
 };
