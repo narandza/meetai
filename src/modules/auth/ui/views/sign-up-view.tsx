56f4133fa80292a7c32297c1890f32602bfc9045
narandza <60319154+narandza@users.noreply.github.com>
2025-06-19 19:45:15 +0200

    build form layout

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -66,6 +66,19 @@ export const SignUpView = () => {
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Let&apos;s get started</h1>
+                  <p className="text-muted-foreground text-balance">
+                    Create your account
+                  </p>
+                </div>
+              </div>
+            </form>
+          </Form>
+
           <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
             <p className="text-2xl font-semibold text-white">Meet.AI</p>
