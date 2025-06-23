3643de6f8b90b5fe9d4bfc5764218e74eb4a8d62
narandza <60319154+narandza@users.noreply.github.com>
2025-06-23 17:39:04 +0200

    test sign up +sign in +sign out = pass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
 export default function Home() {
-  return <div>Home page</div>;
+  const { data: session } = authClient.useSession();
+  return (
+    <div>
+      <p>Logged in as {session?.user.name}</p>
+
+      <Button onClick={() => authClient.signOut()}>Sign out</Button>
+    </div>
+  );
 }
