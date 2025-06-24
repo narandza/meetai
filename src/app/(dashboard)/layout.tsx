4df65c254e34eeae9129553710e30a923ffc0a2e
narandza <60319154+narandza@users.noreply.github.com>
2025-06-24 18:41:16 +0200

    build main layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,18 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: Props) => {
-  return <div className="">{children}</div>;
+  return (
+    <SidebarProvider>
+      {/* TODO: <DashcboardSidebar /> */}
+      <main className="flex flex-col h-screen w-screen bg-muted">
+        {children}
+      </main>
+    </SidebarProvider>
+  );
 };
 
 export default Layout;
